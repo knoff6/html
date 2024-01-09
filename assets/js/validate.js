b0441354afdd7198d9580e67e56ec9b5185a9746
knoff6 <156083220+knoff6@users.noreply.github.com>
2024-01-09 18:01:10 +0530

    Initial

---
+++ assets/js/validate.js
@@ -0,0 +1,14 @@
+function validateLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var messageContainer = document.getElementById("login-message");
+
+  if (username === "Nixiehackoutflag1dirtym13nd" && password === "Youcans33m3") {
+    messageContainer.innerHTML = "<p>Login successful!</p>";
+    window.location.href = "/pages/challenge.html";
+    return false; 
+  } else {
+    messageContainer.innerHTML = '<p style="color: white;">Username or password is incorrect.</p>';
+    return false; 
+  }
+}
